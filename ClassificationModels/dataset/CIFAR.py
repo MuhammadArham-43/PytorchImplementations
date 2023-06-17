af3771ad2c6647494ed660f0dc9aa70d9cfb7aa0
Muhammad Arham <iqbalarham43@gmail.com>
2023-06-17 12:18:04 -0500

    Fixed basic errors

--- ClassificationModels/dataset/CIFAR.py
+++ ClassificationModels/dataset/CIFAR.py
@@ -8,18 +8,18 @@ import numpy as np
 
 
 class CIFARDataset(Dataset):
-    def __init__(self, train: bool = True, image_size=(512,512)) -> None:
+    def __init__(self, train: bool = True, image_size=(512, 512)) -> None:
         super(CIFARDataset, self).__init__()
         self.cifar = CIFAR10(
             root="data/cifar",
             train=train,
             download=True,
-            transform=Compose([Resize(image_size), ToTensor(), Normalize(mean=(0.5,), std=(0.5,))]),
+            transform=Compose([Resize(image_size), ToTensor(),
+                              Normalize(mean=(0.5,), std=(0.5,))]),
         )
 
     def __len__(self):
-        # return len(self.cifar)
-        return 100
+        return len(self.cifar)
 
     def __getitem__(self, index):
         img, label = self.cifar[index]
@@ -40,10 +40,10 @@ if __name__ == "__main__":
     for i in range(4):
         # print(images[i].shape, labels[i].item())
         fig.add_subplot(rows, cols, i + 1)
-        
+
         image = images[i].squeeze().detach().cpu().numpy()
         image = np.moveaxis(image, 0, 2)
-        
+
         plt.imshow(image)
         plt.title(labels[i].item())
 
